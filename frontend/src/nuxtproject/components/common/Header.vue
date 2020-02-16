12bc142cc5d3e299b1d0666db8d135c0511a6417
michio-k <ec17a05@gmail.com>
2020-02-16 17:16:19 +0900

    temp

--- frontend/src/nuxtproject/components/common/Header.vue
+++ frontend/src/nuxtproject/components/common/Header.vue
@@ -1,20 +1,6 @@
 <template>
-  <v-app-bar app clipped-left color="green" dense>
-    <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-    <v-icon class="mx-4">fab fa-youtube</v-icon>
-    <v-toolbar-title class="mr-12 align-center">
-      <span class="title"><nuxt-link to="/">食事管理アプリ</nuxt-link></span>
-    </v-toolbar-title>
-    <v-spacer />
-    <v-row align="center" style="max-width: 500px">
-      <v-text-field
-        :append-icon-cb="() => {}"
-        placeholder="Search..."
-        single-line
-        append-icon="search"
-        color="white"
-        hide-details
-      />
-    </v-row>
+  <v-app-bar app color="indigo" dark>
+    <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+    <v-toolbar-title>Application</v-toolbar-title>
   </v-app-bar>
 </template>
--- frontend/src/nuxtproject/components/common/Navigation.vue
+++ frontend/src/nuxtproject/components/common/Navigation.vue
@@ -1,47 +1,21 @@
 <template>
-  <v-navigation-drawer v-model="drawer" app clipped>
+  <v-navigation-drawer v-model="drawer" app>
     <v-list dense>
-      <v-list-item v-for="item in items" :key="item.text" link>
+      <v-list-item @click="">
         <v-list-item-action>
-          <v-icon>{{ item.icon }}</v-icon>
+          <v-icon>home</v-icon>
         </v-list-item-action>
         <v-list-item-content>
-          <v-list-item-title>
-            {{ item.text }}
-          </v-list-item-title>
+          <v-list-item-title>Home</v-list-item-title>
         </v-list-item-content>
       </v-list-item>
-      <v-subheader class="mt-4 grey--text text--darken-1"
-        >SUBSCRIPTIONS</v-subheader
-      >
-      <v-list>
-        <v-list-item v-for="item in items2" :key="item.text" link>
-          <v-list-item-avatar>
-            <img
-              :src="
-                `https://randomuser.me/api/portraits/men/${item.picture}.jpg`
-              "
-              alt=""
-            />
-          </v-list-item-avatar>
-          <v-list-item-title v-text="item.text" />
-        </v-list-item>
-      </v-list>
-      <v-list-item class="mt-4" link>
+      <v-list-item @click="">
         <v-list-item-action>
-          <v-icon color="grey darken-1">mdi-plus-circle-outline</v-icon>
+          <v-icon>contact_mail</v-icon>
         </v-list-item-action>
-        <v-list-item-title class="grey--text text--darken-1"
-          >Browse Channels</v-list-item-title
-        >
-      </v-list-item>
-      <v-list-item link>
-        <v-list-item-action>
-          <v-icon color="grey darken-1">mdi-settings</v-icon>
-        </v-list-item-action>
-        <v-list-item-title class="grey--text text--darken-1"
-          >Manage Subscriptions</v-list-item-title
-        >
+        <v-list-item-content>
+          <v-list-item-title>Contact</v-list-item-title>
+        </v-list-item-content>
       </v-list-item>
     </v-list>
   </v-navigation-drawer>
@@ -50,21 +24,7 @@
 <script>
 export default {
   data: () => ({
-    drawer: null,
-    items: [
-      { icon: 'trending_up', text: 'Most Popular' },
-      { icon: 'subscriptions', text: 'Subscriptions' },
-      { icon: 'history', text: 'History' },
-      { icon: 'featured_play_list', text: 'Playlists' },
-      { icon: 'watch_later', text: 'Watch Later' }
-    ],
-    items2: [
-      { picture: 28, text: 'Joseph' },
-      { picture: 38, text: 'Apple' },
-      { picture: 48, text: 'Xbox Ahoy' },
-      { picture: 58, text: 'Nokia' },
-      { picture: 78, text: 'MKBHD' }
-    ]
+    drawer: null
   })
 }
 </script>
